de4112eec4295ea20fc1dda14f80369a2b7dc888
mzkmnk <sma0729mex@gmail.com>
2024-12-25 22:35:50 +0900

    chore:app.componentリファクタ

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'with-storage-sync',
+    loadComponent:() => import('./pages/with-storage-sync/with-storage-sync.component').then((M) => M.WithStorageSyncComponent),
+  }
+];
