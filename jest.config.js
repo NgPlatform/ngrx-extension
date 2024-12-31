df02a0da26df7f666ce6e4724af6ecfc58061e87
mizuki <114119396+mzkmnk@users.noreply.github.com>
2024-12-31 19:04:36 +0900

    Fix/prefix testing with storage sync (#3)
    * fix:projectsに移動
    
    * fix:import修正
    
    * fix:ngrx-storage-testing
    
    * chore:en->ja

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@
 // https://kakehashi-dev.hatenablog.com/entry/2022/07/06/090000
 
 module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-    // globalSetup: 'jest-preset-angular/global-setup',
-    testMatch: ['<rootDir>/src/app/libs/**/*.spec.ts'],
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  // globalSetup: 'jest-preset-angular/global-setup',
+  testMatch: ['<rootDir>/projects/ngrx-extension/src/lib/**/*.spec.ts'],
 };
